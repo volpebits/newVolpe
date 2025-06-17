4230d5bb299abfedd8c6596f0800872912a11707
Ziltonbarreto <ziltonbarreto@outlook.com>
2025-06-17 16:16:54 -0300

    feath:Build a new footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import { ThemeProvider } from './components/ThemeContext'
 import Rybena from './components/libras'
@@ -13,12 +14,13 @@ export default function RootLayout({ children }) {
         <html lang="pt-BR" suppressHydrationWarning={true}>
             <body suppressHydrationWarning={true}>
                 <ThemeProvider>
-                    <div className="min-h-screen w-full bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-200">
+                    <div className="min-h-screen w-full bg-gray-50 vh-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-200">
                         <Header />
-                        <main className="mx-auto">
+                        <main className="mx-auto vh-100">
                             {children}
                         </main>
                     </div>
+                    <Footer />
                     <Rybena />
                 </ThemeProvider>
             </body>
